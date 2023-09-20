e3c63212019a71016c625ad6fdaa7569e71e9745
Bruno Bonassi Montico <126211530+BrunoBmontico@users.noreply.github.com>
2023-09-20 18:00:23 -0300

    Add files via upload

---
+++ homePage/JS/home_page.js
@@ -0,0 +1,24 @@
+const header = document.querySelector('header');
+let loader = document.getElementById('preloader');
+
+/*load do site*/
+window.addEventListener('load', function(){
+    loader.style.display = 'none';
+});
+
+/*header*/
+window.addEventListener('scroll', function(){
+    if (window.scrollY > 100 && header.classList.contains('header_home')){
+        header.classList.remove('header_home');
+        header.classList.add('header_scrolled');
+        document.getElementById('logo_pink').style.width = '200px'
+        document.getElementById('perfil').style.width = '50px'
+        document.getElementById('perfil').style.height = '50px'
+    }else if (window.scrollY == 0 && header.classList.contains('header_scrolled')){
+        header.classList.add('header_home');
+        header.classList.remove('header_scrolled');
+        document.getElementById('logo_pink').style.width = '300px'
+        document.getElementById('perfil').style.width = '60px'
+        document.getElementById('perfil').style.height = '60px'
+    }
+});
